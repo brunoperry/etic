857654f7a00030a7bf3733cdc6c2cfc28a1b6bba
Bruno Perry <brunoperry@gmail.com>
2024-01-26 20:35:08 +0000

    update

--- exercicio1/app.js
+++ exercicio1/app.js
@@ -1,67 +1,66 @@
-/**
- * -- Declaração e Manipulação de Variáveis --
- * Declara duas variáveis, a e b, e atribui valores a elas. 
- * Troca os valores de a e b sem usar uma variável temporária. 
- * Faz console.log dessas 2 variáveis.
- */
+window.onload = () => {
+    /**
+     * -- Declaração e Manipulação de Variáveis --
+     * Declara duas variáveis, (a , b) e atribui-lhes um valor.
+     * Troca os valores de a e b sem usar uma variável temporária.
+     * Faz console.log dessas 2 variáveis.
+     */
 
 
 
-/**
- * -- Instruções Condicionais --
- * Escreve uma função (isEven, por ex.) que recebe um parâmetro numérico.
- * O resultado dessa função verifica se o número é par ou ímpar. 
- * Faz console.log desse resultado.
- */
+    /**
+     * -- Instruções Condicionais --
+     * Escreve uma função (isEven, por ex.) que recebe um parâmetro numérico.
+     * O resultado dessa função verifica se o número é par ou ímpar.
+     * Faz console.log desse resultado.
+     */
 
 
 
-/**
- * -- Arrays --
- * Escreve uma função que recebe um array de 5 números (por ex: [2, 123, 34, 65, 48]). 
- * Itera sobre esse array e faz console.log apenas dos números pares.
- */
+    /**
+     * -- Arrays --
+     * Escreve uma função que recebe um array de 5 números (por ex: [2, 123, 34, 65, 48]).
+     * Itera sobre esse array e faz console.log apenas dos números pares.
+     */
 
 
 
-/**
- * -- Funções --
- * Escreve uma função que recebe dois parâmetros, num1, num2, e retorna a soma deles. 
- * Faz console.log desse resultado.
- */
+    /**
+     * -- Funções --
+     * Escreve uma função que recebe dois parâmetros, num1, num2, e retorna a soma deles.
+     * Faz console.log desse resultado.
+     */
 
 
 
-/**
- * -- Objetos --
- * Cria um objeto que represente uma pessoa com as  propriedades nome, idade e genero. 
- * Faz console.log dessas propriedades.
- */
+    /**
+     * -- Objetos --
+     * Cria um objeto que represente uma pessoa com as  propriedades nome, idade e genero.
+     * Faz console.log dessas propriedades.
+     */
 
 
 
-/**
- * -- Loops --
- * Escreve um for loop que imprime a sequência de Fibonacci até um número específico de items (10, por ex.).
- * Faz console.log a cada iteração.
- */
+    /**
+     * -- Loops --
+     * Escreve um for loop que imprime a sequência de Fibonacci até um número específico de items (10, por ex.).
+     * Faz console.log a cada iteração.
+     */
 
 
 
-/**
- * -- Manipulação do DOM --
- * No ficheiro index.html, cria dois elementos do tipo <button> e <p> dentro do  <body>. 
- * Cria um evento onclick no botão que quando “clicado”, insere o texto “button clicked” no elemento <p>.
- */
+    /**
+     * -- Manipulação do DOM --
+     * No ficheiro index.html, cria dois elementos do tipo <button> e <p> dentro do  <body>.
+     * Cria um evento onclick no botão que quando “clicado”, insere o texto “button clicked” no elemento <p>.
+     */
 
 
 
-/**
- * -- Async/Await e Fetch --
- * Cria uma função (loadData, por ex) que faça fetch do ficheiro “data.json”
- * e transforme o resultado desse fetch em formato json. 
- * Faz console.log desse json.
- */
-
-
-
+    /**
+     * -- Async/Await e Fetch --
+     * Cria uma função (loadData, por ex) que faça fetch do ficheiro “data.json”
+     * e transforme o resultado desse fetch em formato json.
+     * Faz console.log desse json.
+     */
+}
